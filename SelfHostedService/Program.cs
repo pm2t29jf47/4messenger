32e97c03289ee29349e856c8070beaaf5fc681e4
pm2t29jf47 <pm2t29jf47@gmail.com>
2012-10-17 14:46:59 +0400

    created self-hosted service

--- SelfHostedService/Program.cs
+++ SelfHostedService/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
+using DBService;
 
 namespace SelfHostedService
 {
@@ -9,6 +11,23 @@ namespace SelfHostedService
     {
         static void Main(string[] args)
         {
+            //Create a URI to serve as the base address
+            Uri httpUrl = new Uri("http://localhost:8090/DBService");
+            //Create ServiceHost
+            ServiceHost host = new ServiceHost(typeof(Service1), httpUrl);
+            //Add a service endpoint
+            //host.AddServiceEndpoint(typeof(MyCalculatorService.ISimpleCalculator)
+           // , new WSHttpBinding(), "");
+            //Enable metadata exchange
+           // ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+          //  smb.HttpGetEnabled = true;
+          //  host.Description.Behaviors.Add(smb);
+            //Start the Service
+            host.Open();
+
+            Console.WriteLine("Service is host at " + DateTime.Now.ToString());
+            Console.WriteLine("Host is running... Press <Enter> key to stop");
+            Console.ReadLine();
         }
     }
 }

1c8722eec53a05c7c4b74764cd7774c01bccd033
pm2t29jf47 <pm2t29jf47@gmail.com>
2012-10-30 20:08:35 +0400

    fix autocomplete issue

--- WPFClient/MessageCreator.xaml.cs
+++ WPFClient/MessageCreator.xaml.cs
@@ -71,7 +71,7 @@ namespace WPFClient
         private void OnRecipientTextboxLostFocus(object sender, RoutedEventArgs e)
         {
             string errorMessage;
-            if (CheckRecipientString(out errorMessage))
+            if (CheckRecipientTextbox(out errorMessage))
             {
                 MessageControl1.RecipientTextbox.Text = string.Empty;
                 foreach (var employee in RecipientsEmployees)
@@ -86,7 +86,7 @@ namespace WPFClient
             }
         }
 
-        private bool CheckRecipientString(out string errorMessage )
+        private bool CheckRecipientTextbox(out string errorMessage )
         {
             RecipientsEmployees.Clear();
             string[] recipientsStringArray = MessageControl1.RecipientTextbox.Text.Split(new char[1] {';'}, StringSplitOptions.RemoveEmptyEntries);
@@ -97,42 +97,69 @@ namespace WPFClient
             }
             foreach (var recipientString in recipientsStringArray)
             {
-                string[] recipientFields = recipientString.Split(new char[1]{' '}, StringSplitOptions.RemoveEmptyEntries);
-                switch (recipientFields.Length)
-                {
-                    case 1:
-                        {
-                            Employee foundEmployee = SearchEmployeesByUsername(recipientString, out errorMessage);
-                            if (foundEmployee != null)
-                                RecipientsEmployees.Add(foundEmployee);
-                            else
-                                return false;
-                            break;
-                        }
-                    case 3:
-                        {
-                            string username;
-                            if (ContainUsername(recipientString, out errorMessage, out username))
-                            {
-                                Employee foundEmployee = SearchEmployeesByUsername(username, out errorMessage);
-                                if (foundEmployee != null)
-                                    RecipientsEmployees.Add(foundEmployee);
-                                else
-                                    return false;
-                            }
-                            else
-                                return false;
-                            break;
-                        }
-                    default:
-                        {
-                            errorMessage = Properties.Resources.InvalidRecipientString + recipientString;
-                            break;
-                        }
-                }
+                if (!CheckRecipientString(recipientString, out errorMessage))
+                    return false;
             }
             errorMessage = string.Empty;
             return true;
+        }
+
+        private bool CheckRecipientString(string recipientString, out string errorMessage)
+        {
+            string[] recipientFields = recipientString.Split(new char[1] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            switch (recipientFields.Length)
+            {
+                case 1:
+                    return ProcessOneWordRecipientString(recipientString, out errorMessage);                    
+                case 3:
+                    return ProcessThreeWordRecipientString(recipientString, out errorMessage);
+                default:
+                        errorMessage = Properties.Resources.InvalidRecipientString + recipientString;
+                        return false;
+            }
+        }
+
+        private bool ProcessOneWordRecipientString(string recipientString, out string errorMessage)
+        {
+            string[] substrings = recipientString.Split(new char[2] { '<', '>' }, StringSplitOptions.RemoveEmptyEntries);
+            recipientString = substrings[0];
+            Employee foundEmployee = AllEmployees.FirstOrDefault(i => (string.Compare(i.Username, recipientString) == 0));
+            if (foundEmployee != null)
+            {
+                RecipientsEmployees.Add(foundEmployee);
+                errorMessage = null;
+                return true;
+            }
+            else
+            {
+                errorMessage = recipientString + " " + Properties.Resources.NotFound;
+                return false;
+            }
+        }
+
+        private bool ProcessThreeWordRecipientString(string recipientString, out string errorMessage)
+        {
+            string username;
+            if (ParseUsername(recipientString, out username))
+            {
+                Employee foundEmployee = AllEmployees.FirstOrDefault(i => (string.Compare(i.Username, username) == 0));
+                if (foundEmployee != null)
+                {
+                    RecipientsEmployees.Add(foundEmployee);
+                    errorMessage = null;
+                    return true;
+                }
+                else
+                {
+                    errorMessage = username + " " + Properties.Resources.NotFound;
+                    return false;
+                }
+            }
+            else
+            {
+                errorMessage = Properties.Resources.InvalidRecipientString + recipientString;
+                return false;
+            }
         }
         
         private string EmployeeToString(Employee employee)
@@ -142,29 +169,17 @@ namespace WPFClient
                     + employee.Username + ">";           
         }
 
-        private Employee SearchEmployeesByUsername(string username, out string errorMessage)
-        {
-            var FoundEmployee = AllEmployees.FirstOrDefault(i => (string.Compare(i.Username, username) == 0));
-            if (FoundEmployee == null)     
-                errorMessage = username + " " + Properties.Resources.NotFound;  
-            else
-                errorMessage = null;
-            return FoundEmployee;            
-        }
-
-        private bool ContainUsername(string recipientString ,out string errorMessage, out string username)
+        private bool ParseUsername(string recipientString , out string username)
         {
             int begin = recipientString.IndexOf('<'),
                             end = recipientString.IndexOf('>');
             if (begin == -1 || end == -1)
             {
-                errorMessage = Properties.Resources.InvalidRecipientString + recipientString;
                 username = null;
                 return false;
             }
             begin += 1;
             username = recipientString.Substring(begin, end - begin);
-            errorMessage = null;
             return true;
         }
     }

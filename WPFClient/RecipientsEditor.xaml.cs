4b62659ba675c2830c0eeb5371138aea8b6e15ae
pm2t29jf47 <pm2t29jf47@gmail.com>
2012-11-02 02:13:13 +0400

    Add RecipientsEditor

--- WPFClient/RecipientsEditor.xaml.cs
+++ WPFClient/RecipientsEditor.xaml.cs
@@ -9,19 +9,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Entities;
 
 namespace WPFClient
 {
     /// <summary>
     /// Interaction logic for RecipientsEditor.xaml
     /// </summary>
-    public partial class RecipientsEditor : UserControl
+    public partial class RecipientsEditor : Window
     {
         public RecipientsEditor()
         {
             InitializeComponent();
+            var a = new List<Employee>();
+            a.Add(new Employee("u1", "f1", "s1", "", ""));
+            a.Add(new Employee("u2", "f2", "s2", "", ""));
+            a.Add(new Employee("u3", "f3", "s3", "", ""));
+            ctrl.AllEmployeesList = a;
         }
     }
 }

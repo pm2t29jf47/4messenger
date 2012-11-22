ccc3776c7865abe4221b5846384d4e37dcbf96a5
pm2t29jf47 <pm2t29jf47@gmail.com>
2012-11-22 19:04:53 +0400

    some code

--- WPFClient/Additional/ServiceWatcher.cs
+++ WPFClient/Additional/ServiceWatcher.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Entities;
+using System.Threading;
 
 namespace WPFClient.Additional
 {
-    class ServiceWatcher
+    public class ServiceWatcher
     {
+        public ServiceWatcher() { }
+
+        public void StartWathing()
+        {///im watching u!
+            allEmployees = App.Proxy.GetAllEmployees();
+            Mutex d = new Mutex()
+
+        }
+  
+        List<Employee> allEmployees;
+    
+
+        public List<Employee> AllEmployees
+        {
+            
+        
+            get;
+            set;
+        
+        }
     }
 }
